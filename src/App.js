1403919af0ec8869a3730664d565553948620f1a
Sing Hao Cheng <chengsh98@gmail.com>
2022-05-09 01:52:11 +1000

    add routing and persistent nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import HomePage from './routes/homepage/homepage.component';
+
+const navigationBar = () => {
+  return (
+    <div>
+      <div>I am nav bar</div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path='/home' element={navigationBar()}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
   );
 }
 
