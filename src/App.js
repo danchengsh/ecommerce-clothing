55b8710fdc692728715956040acb8355453b9771
Sing Hao Cheng <chengsh98@gmail.com>
2022-06-11 00:00:34 +1000

    create sign in page and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './routes/homepage/homepage.component';
+import SignIn from './routes/sign-in/sign-in.component';
 import NavigationBar from './routes/navigationbar/navigationbar.component';
 
 const Shop = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <Route path='/' element={<NavigationBar />}>
         <Route index element={<HomePage />} />
         <Route path='shop' element={Shop()} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
