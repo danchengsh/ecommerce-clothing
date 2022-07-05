1f0736af4142349fbe1eb0d0f6f0a68d08d80f71
Sing Hao Cheng <chengsh98@gmail.com>
2022-07-05 17:49:58 +1000

    rename CategoryItem to DirectoryItem to fix clashing styles, add navigation from Shop to Category pages

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,13 +1,13 @@
+import { Link } from 'react-router-dom';
 import ProductCard from "../product-card/product-card.component";
 
 import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
-
     return (
         <div className="category-preview-container">
             <h2>
-                <span className="title">{title}</span>
+                <Link className="title" to={title}>{title.toUpperCase()}</Link>
             </h2>
             <div className="preview">
                 {products.filter((_, index) => index < 4).map((product) => {
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 //another way of doing this is using a Class Component cos we wanna store the state value of those menu items that we wanna display.
@@ -6,7 +6,7 @@ const Directory = ({ categories }) => {
     return (
         <div className="directory-menu">
             {categories.map((category) => (
-                <CategoryItem key={category.id} category={category} />
+                <DirectoryItem key={category.id} category={category} />
             ))}
         </div>
     );
