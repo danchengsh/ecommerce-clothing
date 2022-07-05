a7542e1befcd6b778f5ef3d2607b0d0e8aacb63a
Sing Hao Cheng <chengsh98@gmail.com>
2022-07-05 16:33:50 +1000

    refactor Directory into functional component, create CategoryItem to replace MenuItem, remove redundant css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './routes/homepage/homepage.component';
 import Authentication from './routes/authentication/authentication.component';
 import NavigationBar from './routes/navigationbar/navigationbar.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+
+import './App.css';
 
 const App = () => {
   return (
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import './menu-item.styles.scss';
+/*
+    deprecated, replaced by CategoryItem
+*/
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`}>        {/* React gives every HTML element a property called "style" that takes an OBJECT that has prop values equal to CSS values that u wanna apply to the element. `url()` with `` means we do string interpolation w value of imageUrl  */}
-        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>  {/* we put styles here cos when we hover n the image expands, we dont want the surrounding container to expand, we only want the pic to expand WITHIN the container */}
+// import React from 'react';
+// import './menu-item.styles.scss';
 
-        </div>
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-)
+// const MenuItem = ({title, imageUrl, size}) => (
+//     <div className={`menu-item ${size}`}>        {/* React gives every HTML element a property called "style" that takes an OBJECT that has prop values equal to CSS values that u wanna apply to the element. `url()` with `` means we do string interpolation w value of imageUrl  */}
+//         <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>  {/* we put styles here cos when we hover n the image expands, we dont want the surrounding container to expand, we only want the pic to expand WITHIN the container */}
 
+//         </div>
+//         <div className="content">
+//             <h1 className="title">{title.toUpperCase()}</h1>
+//             <span className="subtitle">SHOP NOW</span>
+//         </div>
+//     </div>
+// )
 
-export default MenuItem;
+// export default MenuItem;
