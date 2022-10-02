a6befde196cc11096b0cd02bcb9079476715a7b4
Sing Hao Cheng <chengsh98@gmail.com>
2022-10-02 14:29:27 +1100

    create globalStyles, added some mobile styles for navigationbar and categorypreview

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Authentication from './routes/authentication/authentication.component';
 import NavigationBar from './routes/navigationbar/navigationbar.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import { GlobalStyle } from './global.styles';
 
-import './App.css';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<NavigationBar />}>
-        <Route index element={<HomePage />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <div>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<NavigationBar />}>
+          <Route index element={<HomePage />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
